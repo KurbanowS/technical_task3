a504263f8047bdce4aa96a6980ebfad4dc7382d7
Sarcha Gurbanow <kurbanovsarca@gmail.com>
2024-02-24 20:55:08 +0500

    fill main.go

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/KurbanowS/technical_task3/config"
+	"github.com/KurbanowS/technical_task3/internal/api"
+	"github.com/KurbanowS/technical_task3/internal/school"
+	"github.com/KurbanowS/technical_task3/internal/school/pgx"
+	"github.com/KurbanowS/technical_task3/internal/utils"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	defer utils.InitLogs().Close()
+	config.LoadConfig()
+	defer school.Init().(*pgx.PgxSchool).Close()
+
+	routes := gin.Default()
+	api.Routes(routes)
+	if err := routes.Run(":8000"); err != nil {
+		log.Fatal(err)
+	}
 }
